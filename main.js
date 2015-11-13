2dc2482192b9fed92f420cd625eafc182387009e
soundyogi <soundyogi@gmail.com>
2015-11-13 12:33:57 +0000

    tween_h1

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ request("/kiss/0001_theponyfoochallenge.md", function(a,b,c){
     
 })
 
+
+
+
+window.onload = function(){
+    var h1 = document.getElementsByTagName("h1")
+    debugger
+    var words = document.getElementsByClassName('word')
+    var tl = new TimelineMax();
+    tl.staggerTo(words, 1, {top:0, ease:Quart.easeOut}, 0.5);
+    
+    var box = document.getElementsByClassName("box")
+    var tl2 = new TimelineMax();
+    tl2.staggerTo(box, 10, {bottom:0, ease:Quart.easeOut}, 5);
+}
+
