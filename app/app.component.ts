e097cf20ff6a9a052c6cb1e62ef438490273f887
soundyogi <soundyogi@gmail.com>
2016-01-21 23:47:53 +0000

    angular2 working with szstemjs

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'angular2',
     template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent {}
